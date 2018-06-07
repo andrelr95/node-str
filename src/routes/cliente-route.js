7f70aa4d8c064ce6843b6687f6f7f1bc3b976261
Andre Ramos <andre.lopes@soaexpert.com.br>
2018-06-07 13:31:04 -0300

    fix endpoint

--- src/routes/cliente-route.js
+++ src/routes/cliente-route.js
@@ -4,6 +4,6 @@ const express = require('express');
 const router = express.Router();
 const controllers = require('./../controllers/cliente-controller');
 
-router.post('/clientes', controllers.post);
+router.post('/', controllers.post);
 
 module.exports = router;
