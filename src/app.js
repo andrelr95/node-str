2e49d81b2e3c9a747e5ee5598af720de20abd320
Andre Ramos <andre.lopes@soaexpert.com.br>
2018-09-09 00:52:51 -0300

    CORS

--- src/app.js
+++ src/app.js
@@ -25,6 +25,12 @@ const clienteRoute = require('./routes/cliente-route');
 const estoqueRoute = require('./routes/estoque-route');
 const authenticateRoute = require('./routes/authenticate-route');
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
